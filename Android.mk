e2e20ccfe4ebc8a4c649a7c45ae57a031fbdcfa4
maniacx <macs18max@gmail.com>
2015-10-03 20:34:57 +0400

    Remove other devices as this is dedicated to qsd8k

--- Android.mk
+++ Android.mk
@@ -1,20 +1,6 @@
 ifeq ($(call my-dir),$(call project-path-for,qcom-audio))
 
 AUDIO_HW_ROOT := $(call my-dir)
-
-ifeq ($(TARGET_BOARD_PLATFORM),msm7x27a)
-    include $(AUDIO_HW_ROOT)/msm7627a/Android.mk
-endif
-ifeq ($(TARGET_BOARD_PLATFORM),msm7x30)
-    include $(AUDIO_HW_ROOT)/msm7x30/Android.mk
-endif
-ifeq ($(TARGET_BOARD_PLATFORM),msm8660)
-    include $(AUDIO_HW_ROOT)/msm8660/Android.mk
-    include $(AUDIO_HW_ROOT)/mm-audio/Android.mk
-endif
-ifeq ($(TARGET_BOARD_PLATFORM),msm8960)
-    include $(AUDIO_HW_ROOT)/mm-audio/Android.mk
-endif
 ifeq ($(TARGET_BOARD_PLATFORM),qsd8k)
     include $(AUDIO_HW_ROOT)/qsd8k/Android.mk
 endif
